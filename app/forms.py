577a22ebe79352ad8402cc81a364cfca297025b2
unknown <martynasvaznonis@gmail.com>
2022-10-29 13:56:40 +0100

    Updated complete and incomplete pages and discared dropdown menus

--- app/forms.py
+++ app/forms.py
@@ -18,11 +18,3 @@ class AssessmentForm(FlaskForm):
     code = StringField('title', validators=[DataRequired(), length_check(20)])
     deadline = DateField('deadline', format='%Y-%m-%d', validators=[DataRequired(), date_check])
     description = TextAreaField('description', validators=[DataRequired(), length_check(1000)])
-
-class ParameterForm(FlaskForm):
-    show = SelectField('show', choices=[('all', 'Showing all'),
-                                        ('complete', 'Showing completed'),
-                                        ('incomplete', 'Showing uncompleted')])
-    sort = SelectField('sort', choices=[('asc', 'Due date (asc)'),
-                                        ('desc', 'Due date (desc)'),
-                                        ('added', 'Date added')])
--- app/views.py
+++ app/views.py
@@ -1,52 +1,63 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from .models import Assessment
-from .forms import AssessmentForm, ParameterForm
+from .forms import AssessmentForm
 
-def complete(list):
-    for id in list:
-        Assessment.query.get(int(id)).complete = True
+def mark_complete(id):
+    Assessment.query.get(int(id)).complete = True
 
-def uncomplete(list):
-    for id in list:
-        Assessment.query.get(int(id)).complete = False
+def mark_uncomplete(id):
+    Assessment.query.get(int(id)).complete = False
 
-def delete(list):
-    for id in list:
-        db.session.delete(Assessment.query.get(int(id)))
+def delete(id):
+    db.session.delete(Assessment.query.get(int(id)))
+
+def button_pressed(button):
+    return button in request.form
+
+def modify_selected(func_name):
+    for id in request.form.getlist('assessment_id'):
+        globals()[func_name](id)
+    db.session.commit()
+
+def base(page, assessments):
+    if button_pressed(f'{page}_button'):
+        modify_selected(request.form[f'{page}_button']) # Button value corresponds to function name
+        return redirect(url_for(page))
+    return render_template(f'{page}.html', assessments=assessments)
+
+def collect_assessments(complete=True, incomplete=True):
+    return sorted([ass for ass in Assessment.query.all()
+                   if ass.complete == complete
+                   or ass.complete != incomplete], key=lambda x: x.deadline)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = ParameterForm()
+    return base('home', collect_assessments())
 
-    if 'home_button' in request.form:
-        globals()[request.form['home_button']](request.form.getlist('assessment_id'))
-        db.session.commit()
-        return redirect(url_for('home'))
-    if 'show_button' in request.form:
-        return redirect(url_for('home'))
-    return render_template('home.html', assessments=Assessment.query.all(), form=form)
+@app.route('/complete', methods=['GET', 'POST'])
+def complete():
+    return base('complete', collect_assessments(incomplete=False))
+
+@app.route('/incomplete', methods=['GET', 'POST'])
+def incomplete():
+    return base('incomplete', collect_assessments(complete=False))
+
+def add_assessment(form):
+    db.session.add(Assessment(
+        title=form.title.data,
+        code=form.code.data,
+        deadline = form.deadline.data,
+        description = form.description.data,
+        complete = False
+    ))
+    db.session.commit()
+    flash('Assessment added successfully')
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_new():
     form = AssessmentForm()
     if form.validate_on_submit():
-        db.session.add(Assessment(
-            title=form.title.data,
-            code=form.code.data,
-            deadline = form.deadline.data,
-            description = form.description.data,
-            complete = False
-        ))
-        db.session.commit()
-        flash('Assessment added successfully')
+        add_assessment(form)
         return redirect(url_for('add_new'))
     return render_template('add.html', title='Add new', form=form)
-
-@app.route('/completed', methods=['GET', 'POST'])
-def completed():
-    return render_template('complete.html', assessments=[x for x in Assessment.query.all() if x.complete])
-
-@app.route('/incomplete', methods=['GET', 'POST'])
-def incomplete():
-    return render_template('incomplete.html', assessments=[x for x in Assessment.query.all() if not x.complete])
